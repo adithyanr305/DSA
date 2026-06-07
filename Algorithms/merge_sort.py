86e9d0695d7e785d58883694ecd202828db84d0e
adithyanr305 <adithyanr346@gmail.com>
2026-06-07 13:28:09 +0530

    Refactor merge sort to support key and order

--- Algorithms/merge_sort.py
+++ Algorithms/merge_sort.py
@@ -1,51 +1,43 @@
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return
+def merge_sort(elements, key, descending=False):
+    size = len(elements)
 
-    mid = len(arr)//2
+    if size == 1:
+        return elements
 
-    left = arr[:mid]
-    right = arr[mid:]
+    left_list = merge_sort(elements[0:size//2], key, descending)
+    right_list = merge_sort(elements[size//2:], key, descending)
+    sorted_list = merge(left_list, right_list, key, descending)
 
-    merge_sort(left)
-    merge_sort(right)
+    return sorted_list
 
-    merge_two_sorted_lists(left, right, arr)
+    
+def merge(left_list, right_list, key, descending=False):
+    merged = []
+    if descending:
+        while len(left_list) > 0 and len(right_list) > 0:
+            if left_list[0][key] >= right_list[0][key]:
+                merged.append(left_list.pop(0))
+            else:
+                merged.append(right_list.pop(0))
 
-def merge_two_sorted_lists(a,b,arr):
-    len_a = len(a)
-    len_b = len(b)
+    else:
+        while len(left_list) > 0 and len(right_list) > 0:
+            if left_list[0][key] <= right_list[0][key]:
+                merged.append(left_list.pop(0))
+            else:
+                merged.append(right_list.pop(0))
 
-    i = j = k = 0
-
-    while i < len_a and j < len_b:
-        if a[i] <= b[j]:
-            arr[k] = a[i]
-            i+=1
-        else:
-            arr[k] = b[j]
-            j+=1
-        k+=1
-
-    while i < len_a:
-        arr[k] = a[i]
-        i+=1
-        k+=1
-
-    while j < len_b:
-        arr[k] = b[j]
-        j+=1
-        k+=1
+    merged.extend(left_list)
+    merged.extend(right_list)
+    return merged
 
 if __name__ == '__main__':
-    test_cases = [
-        [10, 3, 15, 7, 8, 23, 98, 29],
-        [],
-        [3],
-        [9,8,7,2],
-        [1,2,3,4,5]
+    elements = [
+        { 'name': 'vedanth',   'age': 17, 'time_hours': 1},
+        { 'name': 'rajab', 'age': 12,  'time_hours': 3},
+        { 'name': 'vignesh',  'age': 21,  'time_hours': 2.5},
+        { 'name': 'chinmay',  'age': 24,  'time_hours': 1.5},
     ]
 
-    for arr in test_cases:
-        merge_sort(arr)
-        print(arr)
+    sorted_list = merge_sort(elements, key='time_hours', descending=True)
+    print(sorted_list)
